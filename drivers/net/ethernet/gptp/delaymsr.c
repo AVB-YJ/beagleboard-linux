1179410e91896495a37c8b3b2e888119d46f483b
Indumathi Duraipandian <indu9086@gmail.com>
2017-06-02 15:44:46 +0200

    skeleton for gptp loadable module

---
+++ drivers/net/ethernet/gptp/delaymsr.c
@@ -0,0 +1,12 @@
+#include "delaymsr.h"
+
+void dmHandleEvent(int evtId)
+{
+	printk(KERN_INFO "gPTP dmHandleEvent");
+}
+
+void dmHandleStateChange(int toState)
+{
+
+}
+
---
+++ drivers/net/ethernet/gptp/delaymsr.h
@@ -0,0 +1,6 @@
+#include <linux/kernel.h>
+
+#include "gptpcmn.h"
+
+void dmHandleEvent(int evtId);
+void dmHandleStateChange(int toState);
